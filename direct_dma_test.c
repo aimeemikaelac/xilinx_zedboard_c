6c3958a59ae3b32d6cb79a5eb8dad069878d4a60
Michael Coughlin <michael.coughlin@colorado.edu>
2015-04-02 06:50:25 +0000

    tested successfully

--- direct_dma_test.c
+++ direct_dma_test.c
@@ -11,7 +11,7 @@
 
 #define SHARED_MEM_BASE 0x1F400000
 
-#define SHARED_MEM_LENGTH 0x10000
+#define SHARED_MEM_LENGTH 0x800000
 
 unsigned getBaseAddress(){
 	return strtoul(BASE_ADDRESS, NULL, 0);
@@ -38,24 +38,33 @@ int main(){
 	int source = SHARED_MEM_BASE;
 	printf("\nShared memory base: %08x", source);
 	int length = SHARED_MEM_LENGTH;
-	shared_memory shared_system_mem = getSharedMemoryArea(source, length);
-//	printf("\nNo segfault");
+	printf("\nShared memory length: %08x", length);
+	shared_memory shared_system_mem = getSharedMemoryArea(source, 0);
+	printf("\nNo segfault");
 
-	int destOffset = length/2;
-	int dest = source + destOffset;
-	for(i = 0; i<SHARED_MEM_LENGTH; i++){
+	//int destOffset = length/2;
+	//int dest = source + destOffset;
+	for(i = 0; i<100; i++){
 		((char*)shared_system_mem->ptr)[i] = 0;
 	}
 
 	((char*)shared_system_mem->ptr)[0] = 0xff;
-	printf("\nData at source address: %02x", ((char*)shared_system_mem->ptr)[0]);
-	printf("\nData at dest address: %02x", ((char*)shared_system_mem->ptr)[1]);
-
+	printf("\nNo segfault");
 	writeSourceAddress(SHARED_MEM_BASE);
 	writeDestinationAddress(SHARED_MEM_BASE + 1);
 	writeSourceAddressValid();
 	writeDestinationAddressValid();
+	for(i=0; i<0xff; i++){
+		((char*)shared_system_mem->ptr)[0] = (char)i;
+		printf("\nData at source address: %02x", ((char*)shared_system_mem->ptr)[0]);
+		printf("\nData at dest address: %02x", ((char*)shared_system_mem->ptr)[1]);
+		writeSourceAddressValid();
+		writeDestinationAddressValid();
+		printf("\nData at source address: %02x - fabric", ((char*)shared_system_mem->ptr)[0]);
+		printf("\nData at dest address: %02x - fabric", ((char*)shared_system_mem->ptr)[1]);
+	}
 
-	printf("\nData at source address: %02x - fabric", ((char*)shared_system_mem->ptr)[0]);
-	printf("\nData at dest address: %02x - fabric", ((char*)shared_system_mem->ptr)[1]);
+	printf("\nNo segfault");
+	printf("\n");
+	cleanupSharedMemoryPointer(shared_system_mem);
 }
