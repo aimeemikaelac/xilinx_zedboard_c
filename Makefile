fdbb0d83b22942c13975da3fe788d39c3ef52f5a
Michael Coughlin <michael.coughlin@colorado.edu>
2015-05-14 10:27:43 +0000

    fixed compile errors and linked against c++ libraries

--- Makefile
+++ Makefile
@@ -1,13 +1,14 @@
-LIBS 		:= -lcrypto -lssl
+LIBS 		:= -lcrypto -lssl -lstdc++
 INCLUDES	:= test_direct_dma_uio_driver user_mmap_driver xilinx_aes_uio_driver xilinx_qam_uio_driver
 INCL		:= $(foreach d, $(INCLUDES), -I$d/)
 DRIVERS		:= $(foreach d, $(INCLUDES), $(wildcard $d/*.c))
 AES 		:= aes_runner.c
 TEST_DIRECT 	:= direct_dma_test.c
 GPIO 		:= gpio-dev-mem-test.c
-QAM		:= qam_runner.c
+QAM		:= qam_runner.cpp
 
 CC		:= gcc
+CPP		:= g++
 
 aes: $(AES)
 	$(CC) -o $@.o $^ $(DRIVERS) $(INCL) $(LIBS)
@@ -19,7 +20,7 @@ gpio: $(GPIO)
 	$(CC) -o $@.o $^ $(DRIVERS) $(INCL) $(LIBS)
 
 qam: $(QAM)
-	$(CC) -0 $@.o $^ $(DRIVERS) $(INCL) $(LIBS)
+	$(CC) -o $@.o $^ $(DRIVERS) $(INCL) $(LIBS)
 
 .PHONY: clean aes test_direct gpio qam
 
--- qam_runner.cpp
+++ qam_runner.cpp
@@ -10,7 +10,7 @@ int main(){
 	ofstream fp_debug ("debug.txt");
 	float f_din;
 	u32 din_i;
-	u32 din_q
+	u32 din_q;
 	u32 ph_xy_i;
 	u32 ph_xy_q;
 
@@ -34,23 +34,23 @@ int main(){
 		ph_xy_i = 0;
 		ph_xy_q = 0;
 
-		XCrec_Set_din_i_V(qam_instance, din_i);
-		XCrec_Set_din_q_V(qam_instance, din_q);
-		XCrec_Set_ph_in_i_V(qam_instance, ph_xy_i);
-		XCrec_Set_ph_in_q_V(qam_instance, ph_xy_q);
+		XCrec_Set_din_i_V(&qam_instance, din_i);
+		XCrec_Set_din_q_V(&qam_instance, din_q);
+		XCrec_Set_ph_in_i_V(&qam_instance, ph_xy_i);
+		XCrec_Set_ph_in_q_V(&qam_instance, ph_xy_q);
 
-		XCrec_Set_control_qam_V(qam_instance, 0);
-		XCrec_Set_control_lf_p(qam_instance, 6);
-		XCrec_Set_control_lf_i(qam_instance, 5);
-		XCrec_Set_control_lf_out_gain(qam_instance, 1);
-		XCrec_Set_control_reg_clr(qam_instance, 0);
-		XCrec_Set_control_reg_init_V(qam_instance, 0);
+		XCrec_Set_control_qam_V(&qam_instance, 0);
+		XCrec_Set_control_lf_p(&qam_instance, 6);
+		XCrec_Set_control_lf_i(&qam_instance, 5);
+		XCrec_Set_control_lf_out_gain(&qam_instance, 1);
+		XCrec_Set_control_reg_clr(&qam_instance, 0);
+		XCrec_Set_control_reg_init_V(&qam_instance, 0);
 
-		dout_mix_i = XCrec_Get_dout_mix_i_V(qam_instance);
-		dout_mix_q = XCrec_Get_dout_mix_q_V(qam_instance);
-		ph_out_i = XCrec_Get_ph_out_i_V(qam_instance);
-		ph_out_q = XCrec_Get_ph_out_q_V(qam_instance);
-		loop_integ = XCrec_Get_loop_integ_V(qam_instance);
+		dout_mix_i = XCrec_Get_dout_mix_i_V(&qam_instance);
+		dout_mix_q = XCrec_Get_dout_mix_q_V(&qam_instance);
+		ph_out_i = XCrec_Get_ph_out_i_V(&qam_instance);
+		ph_out_q = XCrec_Get_ph_out_q_V(&qam_instance);
+		loop_integ = XCrec_Get_loop_integ_V(&qam_instance);
 
 		fp_dout << dout_mix_i << "\t" << dout_mix_q << "\t" << " " << endl;
 		fp_debug << loop_integ << "\t" << endl;
@@ -60,5 +60,5 @@ int main(){
 	fp_debug.close();
 	fp_cin.close();
 
-	XCrec_Release(qam_instance);
+	XCrec_Release(&qam_instance);
 }
